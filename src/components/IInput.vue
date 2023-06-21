95f3827299446ea4fa07b5e3243ca7a301c8ac7a
pltnv <anton.pletnew@mail.ru>
2023-06-21 21:04:41 +0300

    chore: updates i-input component

--- src/components/IInput.vue
+++ src/components/IInput.vue
@@ -10,7 +10,7 @@ const props = defineProps({
   readonly: Boolean,
   maxlength: String,
   placeholder: String,
-  required: String,
+  required: Boolean,
   showError: Boolean,
   error: String,
   label: String,
@@ -42,7 +42,7 @@ const props = defineProps({
   block: Boolean
 });
 
-const emit = defineEmits(["update:modelValue", "blur"]);
+const emit = defineEmits(["update:modelValue", "blur", "focus"]);
 
 let localValue = ref(props.modelValue);
 let localType = ref(props.type);
@@ -63,7 +63,7 @@ let passwordIcon = computed(() => {
 
 let classes = computed(() => {
   return [
-    `i-input--${props.variant}`,
+    `i-input__wrapper--${props.variant}`,
     {
       error: props.error && !props.disabled,
       "i-input--disabled": props.disabled,
@@ -78,6 +78,10 @@ let updateValue = (newValue) => {
 
 let handleBlur = () => {
   emit("blur");
+};
+
+let handleFocus = () => {
+  emit("focus");
 };
 
 let clear = () => {
@@ -105,125 +109,136 @@ watch(
 </script>
 
 <template>
-  <div class="i-input" :class="classes">
-    <input
-      :value="localValue"
-      :maxlength="maxlength"
-      :autofocus="autofocus"
-      :disabled="disabled"
-      :required="required"
-      :placeholder="placeholder"
-      :readonly="readonly"
-      :type="localType"
-      @blur="handleBlur"
-      @input="updateValue($event.target.value)"
-    />
-    <div v-if="clearable && localValue.length && !disabled && !readonly">
-      <i class="mdi mdi-close" @click="clear" />
+  <div class="i-input">
+    <div class="i-input__label">
+      <div v-if="label" v-text="label" />
+      <sup v-if="required">*</sup>
     </div>
+    <div class="i-input__wrapper" :class="classes">
+      <input
+        :value="localValue"
+        :maxlength="maxlength"
+        :autofocus="autofocus"
+        :disabled="disabled"
+        :required="required"
+        :placeholder="placeholder"
+        :readonly="readonly"
+        :type="localType"
+        @blur="handleBlur"
+        @input="updateValue($event.target.value)"
+        @focus="handleFocus"
+      />
+      <div v-if="clearable && localValue.length && !disabled && !readonly">
+        <i class="mdi mdi-close" @click="clear" />
+      </div>
 
-    <div class="info">
-      <span v-if="count && amount > 0" v-text="amount" />
-      <span v-if="count && maxlength"> / </span>
-      <span v-if="maxlength" v-text="maxlength" />
-    </div>
-    <div v-if="isTypePassword && !disabled">
-      <i class="mdi" :class="passwordIcon" @click="toggleShowPassword" />
+      <div class="info">
+        <span v-if="count && amount > 0" v-text="amount" />
+        <span v-if="count && maxlength"> / </span>
+        <span v-if="maxlength" v-text="maxlength" />
+      </div>
+      <div v-if="isTypePassword && !disabled">
+        <i class="mdi" :class="passwordIcon" @click="toggleShowPassword" />
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
 .i-input {
-  display: flex;
-  align-items: center;
-  gap: 4px;
-  position: relative;
   width: 280px;
-  height: 30px;
-  padding: 6px;
-  border: 1px solid transparent;
-  border-radius: 10px;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 1px 2px;
-
-  &.error {
-    border: 1px solid;
-    border-color: rgb(242, 48, 48);
-    box-shadow: none;
-  }
 
   &--block {
     width: 100%;
   }
 
-  &--outlined {
-    border: 1px solid gray;
-    box-shadow: none;
-
-    &:focus {
-      border: none;
-    }
-
-    &:hover {
-      border: 1px solid black;
-    }
-
-    &.error {
-      border-color: rgb(242, 48, 48);
-    }
-  }
-
-  &--underline {
-    border-bottom: 1px solid gray;
-    border-radius: 0;
-    box-shadow: none;
-
-    :focus-within {
-      border-color: black;
-    }
-
-    &:hover {
-      border-bottom: 1px solid black;
-    }
-
-    &.error {
-      border: none;
-      border-bottom: 1px solid rgb(242, 48, 48);
-    }
-  }
-
-  &--disabled {
-    opacity: 0.5;
-    pointer-events: none;
-  }
-
-  &--readonly {
-    pointer-events: none;
-  }
-
-  input {
-    width: 100%;
-    height: 100%;
-    outline: none;
-    border: none;
-    overflow: hidden;
-    white-space: nowrap;
-  }
-
-  .clear {
-    border: none;
-    background: none;
-    cursor: pointer;
-  }
-
-  .info {
+  &__wrapper {
     display: flex;
     align-items: center;
-    color: #676161;
-  }
+    gap: 4px;
+    position: relative;
+    width: 100%;
+    height: 30px;
+    padding: 6px;
+    border: 1px solid transparent;
+    border-radius: 10px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 1px 2px;
 
-  i {
-    cursor: pointer;
+    &.error {
+      border: 1px solid;
+      border-color: rgb(242, 48, 48);
+      box-shadow: none;
+    }
+
+    &--outlined {
+      border: 1px solid gray;
+      box-shadow: none;
+
+      &:focus {
+        border: none;
+      }
+
+      &:hover {
+        border: 1px solid black;
+      }
+
+      &.error {
+        border-color: rgb(242, 48, 48);
+      }
+    }
+
+    &--underline {
+      border-bottom: 1px solid gray;
+      border-radius: 0;
+      box-shadow: none;
+
+      :focus-within {
+        border-color: black;
+      }
+
+      &:hover {
+        border-bottom: 1px solid black;
+      }
+
+      &.error {
+        border: none;
+        border-bottom: 1px solid rgb(242, 48, 48);
+      }
+    }
+
+    &--disabled {
+      opacity: 0.5;
+      pointer-events: none;
+    }
+
+    &--readonly {
+      pointer-events: none;
+    }
+
+    input {
+      width: 100%;
+      height: 100%;
+      outline: none;
+      border: none;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+
+    .clear {
+      border: none;
+      background: none;
+      cursor: pointer;
+    }
+
+    .info {
+      display: flex;
+      align-items: center;
+      color: #676161;
+    }
+
+    i {
+      cursor: pointer;
+    }
   }
 }
 </style>
