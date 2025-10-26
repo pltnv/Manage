fa75832684b505a2f49f36426967492c3ccbe03b
pltnv <anton.pletnew@mail.ru>
2025-10-26 15:29:57 +0300

    fix: store destructurization

--- src/views/Board/composables/useBoards.js
+++ src/views/Board/composables/useBoards.js
@@ -1,13 +1,13 @@
 import { computed } from "vue";
 import { useRoute } from "vue-router";
+import { storeToRefs } from "pinia";
 import { usePersonalTaskStore } from "@/stores/personalTaskStore";
 
 export function useBoards() {
   const route = useRoute();
   const personalTaskStore = usePersonalTaskStore();
 
-  // add storeToRefs thing
-  const { boards } = personalTaskStore;
+  const { boards } = storeToRefs(personalTaskStore);
 
   const currentBoardIndex = computed(() => route.params.id);
   const boardName = computed(() => route.query?.board);
